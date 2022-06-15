7e32c9034fce83c84a84f796c8ed478d80e9aa08
inAsees <gurasees.singh121@gmail.com>
2022-06-15 14:21:58 +0530

    index.html ,added to dir(templates). 1. in upload_vid/urls.py , url path added. 2. in video_uploader/urls.py , upload_vid.urls path included. 3. in upload_vid/views.py , view(extract_srt) created.

--- video_uploader/upload_vid/views.py
+++ video_uploader/upload_vid/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def extract_srt(request):
+    if request.method == "GET":
+        return render(request, 'index.html')
+
+    video_file = request.FILES["video_file"]
