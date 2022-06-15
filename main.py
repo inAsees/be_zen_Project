3d8b3f70645fb3efa390e587aa5d03704c983e93
inAsees <gurasees.singh121@gmail.com>
2022-06-16 02:25:24 +0530

    files deleted.

--- main.py
+++
@@ -1,7 +0,0 @@
-from backend.src.subtitle_extractor import SubtitleExtractor
-
-
-if __name__ == '__main__':
-    extractor = SubtitleExtractor()
-    extractor.extract_srt_file()
-    print(extractor.get_time_stamps("what?"))
