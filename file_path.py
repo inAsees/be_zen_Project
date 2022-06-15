bdc2f0e25c58c3e49b10b343f0b05ab01ef7b451
inAsees <gurasees.singh121@gmail.com>
2022-06-16 02:24:49 +0530

    1. in views.py , code refactored. 2. new dir(backend_core) created and inside it new module(subtitle_extractor) created. 3. index.html updated. 4. new file(file_path.py) , created in the root.

---
+++ file_path.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent
+
+srt_file = base_dir / "srt_files"
--- video_uploader/upload_vid/views.py
+++ video_uploader/upload_vid/views.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from backend_core.subtitle_extractor import SubtitleExtractor
 from .forms import VideoForm
 
 
@@ -11,5 +14,13 @@ def extract_srt(request):
     if not form.is_valid():
         form = VideoForm()
         return render(request, "index.html", {"form": form})
+    file_path = Path(form.cleaned_data['myfile'].temporary_file_path())
+    file_name = Path(form.cleaned_data['myfile'].name).stem
+    sub_extractor = SubtitleExtractor(str(file_path), file_name)
+    sub_extractor.extract_srt_file()
+    if not sub_extractor.is_subprocess_successful():
+        messages.error(request,  "Subtitle extraction failed. Please try again or make sure video contain subtitles.")
+        return redirect(extract_srt)
+    messages.success(request, "Subtitle extraction successful. Please wait while we upload your video to S3 Bucket.")
     form.save()
     return redirect(extract_srt)
