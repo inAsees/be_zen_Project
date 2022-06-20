8a51e454af91ebd63c44adc06ad6c0a969ba1d28
inAsees <gurasees.singh121@gmail.com>
2022-06-20 12:30:46 +0530

    Some file deleted:  a. upload_data.py  b. src_path.py from ccextractor_win  c. subtitle_search.html  d. index.html  e. handle_video_file.py(moved to tasks.py)  f. file_path.py  e. backend_core/__init__

--- ccextractor_win/src_path.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-base_dir = Path(__file__).resolve().parent
-
-exe_file_path = base_dir / "ccextractorwinfull.exe"
--- file_path.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-base_dir = Path(__file__).resolve().parent
-
-dir_name = base_dir / "srt_files"
