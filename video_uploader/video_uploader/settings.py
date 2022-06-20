80d20ce9a53133106eaed39cfe030b94d4f9d311
inAsees <gurasees.singh121@gmail.com>
2022-06-20 20:50:20 +0530

    minor change in settings.py

--- video_uploader/video_uploader/settings.py
+++ video_uploader/video_uploader/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-x0w04nrg(8bv&2e6$rx4k!0@wrl$8c3&&0)a3g&&d&ixb8m=^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
